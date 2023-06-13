d6c4990065a5b2174ff0c36efc57d2f343746548
Bence Kozma <bencejob@gmail.com>
2023-06-13 20:05:15 +0100

    update tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,6 +9,8 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+    protected $loadEnvironmentVariables = false;
+
     protected function getPackageProviders($app)
     {
         return [
